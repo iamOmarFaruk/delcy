e6dd87126870414dd1efd7074742dfc0f6d97726
Omar Faruk <omarbg.bd@gmail.com>
2024-02-24 11:36:21 +0600

    Update scroll indicators and padding in HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -29,71 +29,79 @@ export default function HomeScreen() {
 
 						{/* categories */}
 						<ScrollView
-							horizontal={true} // Enables horizontal scroll
-							showsHorizontalScrollIndicator={false} // Hides the scroll indicator
-							contentContainerStyle={{
-								paddingRight: 10, // Adds padding at the end if needed
-								paddingBottom: 90,
-							}}
-						>
-							<View style={{ flexDirection: "row", paddingHorizontal: 5 }}>
-								<FoodCategory
-									boxcolor="#eccc68"
-									title="Lunch"
-									imageUri={require("../../assets/images/welcome.png")}
-								/>
-								<FoodCategory
-									boxcolor="#747d8c"
-									title="Dinner"
-									imageUri={require("../../assets/images/welcome.png")}
-								/>
-								<FoodCategory
-									boxcolor="#ff4757"
-									title="Dinner"
-									imageUri={require("../../assets/images/welcome.png")}
-								/>
-								<FoodCategory
-									boxcolor="#70a1ff"
-									title="Dinner"
-									imageUri={require("../../assets/images/welcome.png")}
-								/>
-							</View>
-						</ScrollView>
-
-						{/* single iteams */}
-						<ScrollView
 							showsVerticalScrollIndicator={false} // Hides the scroll indicator
 							contentContainerStyle={{
-								paddingBottom: 560, // Adds padding at the end if needed
-								paddingTop: 20,
+								paddingBottom: 5, // Adds padding at the end if needed
+								paddingTop: 10,
 							}}
 						>
-							<SingleItem
-								title="Mushroom Pizza"
-								weight="410g"
-								price="$14.99"
-								imageUrl={require("../../assets/images/welcome.png")}
-							/>
+							<ScrollView
+								horizontal={true} // Enables horizontal scroll
+								showsHorizontalScrollIndicator={false} // Hides the scroll indicator
+								contentContainerStyle={{
+									paddingRight: 10, // Adds padding at the end if needed
+									paddingBottom: 10,
+								}}
+							>
+								<View style={{ flexDirection: "row", paddingHorizontal: 5 }}>
+									<FoodCategory
+										boxcolor="#eccc68"
+										title="Lunch"
+										imageUri={require("../../assets/images/welcome.png")}
+									/>
+									<FoodCategory
+										boxcolor="#747d8c"
+										title="Dinner"
+										imageUri={require("../../assets/images/welcome.png")}
+									/>
+									<FoodCategory
+										boxcolor="#ff4757"
+										title="Dinner"
+										imageUri={require("../../assets/images/welcome.png")}
+									/>
+									<FoodCategory
+										boxcolor="#70a1ff"
+										title="Dinner"
+										imageUri={require("../../assets/images/welcome.png")}
+									/>
+								</View>
+							</ScrollView>
 
-							<SingleItem
-								title="Mushroom Pizza"
-								weight="410g"
-								price="$14.99"
-								imageUrl={require("../../assets/images/welcome.png")}
-							/>
+							{/* single iteams */}
+							<ScrollView
+								showsVerticalScrollIndicator={false} // Hides the scroll indicator
+								contentContainerStyle={{
+									paddingBottom: 400, // Adds padding at the end if needed
+									paddingTop: 10,
+								}}
+							>
+								<SingleItem
+									title="Mushroom Pizza"
+									weight="410g"
+									price="$14.99"
+									imageUrl={require("../../assets/images/welcome.png")}
+								/>
 
-							<SingleItem
-								title="Mushroom Pizza"
-								weight="410g"
-								price="$14.99"
-								imageUrl={require("../../assets/images/welcome.png")}
-							/>
-							<SingleItem
-								title="Mushroom Pizza"
-								weight="410g"
-								price="$14.99"
-								imageUrl={require("../../assets/images/welcome.png")}
-							/>
+								<SingleItem
+									title="Mushroom Pizza"
+									weight="410g"
+									price="$14.99"
+									imageUrl={require("../../assets/images/welcome.png")}
+								/>
+
+								<SingleItem
+									title="Mushroom Pizza"
+									weight="410g"
+									price="$14.99"
+									imageUrl={require("../../assets/images/welcome.png")}
+								/>
+								<SingleItem
+									title="Mushroom Pizza"
+									weight="410g"
+									price="$14.99"
+									imageUrl={require("../../assets/images/welcome.png")}
+								/>
+							</ScrollView>
 						</ScrollView>
 					</View>
 					<CartButton buttonText="Go to Cart" price="$24.99" itemCount="3" />
